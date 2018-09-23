21ec2d35f252a28b7ad731254399f8ee2262c518
bobrzzz bobrzzz <bobrz-z-z@mail.ru>
2018-09-23 22:13:35 +0200

    New exchange rates component
    Master component to organize form and table components properly

---
+++ src/app/exchange-rates/exchange-rates.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './../api.service';
+
+import { Currency } from '../dataModels/currency';
+import { InternalRates } from '../dataModels/internalRate';
+
+
+@Component({
+  selector: 'app-exchange-rates',
+  templateUrl: './exchange-rates.component.html',
+  styleUrls: ['./exchange-rates.component.css']
+})
+export class ExchangeRatesComponent implements OnInit {
+  private currencies: Currency[];
+  private rates: InternalRates[];
+
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit() {
+    this.currencies = this.apiService.getCurrencies();
+  }
+
+  getRates(e): void {
+    console.log(e)
+    this.apiService.getRates(e.base.code, e.date).subscribe(data => {
+      console.log(data);
+      this.rates = data;
+    });
+  }
+
+}
