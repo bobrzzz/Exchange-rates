c34579c66e92ab7fe54631f2b2ea0966f54477fe
Hleb Koshal <hkoshal@techona.com>
2018-09-24 10:24:36 +0200

    Form test setup

--- src/app/exchange-rates/form/form.component.spec.ts
+++ src/app/exchange-rates/form/form.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FormComponent } from './form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { DatePipe } from '@angular/common';
+import { MOCKUP_currencies } from './../../../mockups/mockCurrency';
 
 describe('FormComponent', () => {
   let component: FormComponent;
@@ -8,14 +11,17 @@ describe('FormComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FormComponent ]
+      declarations: [FormComponent],
+      imports: [ReactiveFormsModule],
+      providers: [DatePipe]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(FormComponent);
     component = fixture.componentInstance;
+    component.currencies = MOCKUP_currencies;
     fixture.detectChanges();
   });
 
