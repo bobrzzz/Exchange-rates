7b2bfeb493da1c3438f0e59c57497545e26f32d3
Hleb Koshal <hkoshal@techona.com>
2018-09-24 10:48:53 +0200

    Api service test setup

--- src/app/api.service.spec.ts
+++ src/app/api.service.spec.ts
@@ -1,9 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ApiService } from './api.service';
+import { DecimalPipe } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('ApiService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientModule],
+    providers: [DecimalPipe]
+  }));
 
   it('should be created', () => {
     const service: ApiService = TestBed.get(ApiService);
