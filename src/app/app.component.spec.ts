cc74eb3787255b3afe56d0e6aa1105317be7280e
Hleb Koshal <hkoshal@techona.com>
2018-09-24 10:44:14 +0200

    App component test setup

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,19 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+
+import { ExchangeRatesComponent } from './exchange-rates/exchange-rates.component';
+import { FormComponent } from './exchange-rates/form/form.component';
+import { TableComponent } from './exchange-rates/table/table.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { DatePipe, DecimalPipe } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent, ExchangeRatesComponent, FormComponent, TableComponent],
+      imports: [ReactiveFormsModule, HttpClientModule],
+      providers: [DatePipe, DecimalPipe]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
@@ -16,12 +24,12 @@ describe('AppComponent', () => {
   it(`should have as title 'exchange-rates'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('exchange-rates');
+    expect(app.title).toEqual('Exchange rates');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to exchange-rates!');
+    expect(compiled.querySelector('h1').textContent).toContain('Exchange rates');
   }));
 });
--- src/app/exchange-rates/exchange-rates.component.spec.ts
+++ src/app/exchange-rates/exchange-rates.component.spec.ts
@@ -9,7 +9,7 @@ import { HttpClientModule } from '@angular/common/http';
 
 
 
-fdescribe('ExchangeRatesComponent', () => {
+describe('ExchangeRatesComponent', () => {
   let component: ExchangeRatesComponent;
   let fixture: ComponentFixture<ExchangeRatesComponent>;
 
