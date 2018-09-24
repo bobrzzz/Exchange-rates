d1d830056c3607d53639fe7cba57583e535c18cb
Hleb Koshal <hkoshal@techona.com>
2018-09-24 10:34:22 +0200

    Exchange rates component test setup

--- src/app/exchange-rates/exchange-rates.component.spec.ts
+++ src/app/exchange-rates/exchange-rates.component.spec.ts
@@ -1,16 +1,25 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ExchangeRatesComponent } from './exchange-rates.component';
+import { FormComponent } from './form/form.component';
+import { TableComponent } from './table/table.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { DatePipe, DecimalPipe } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
-describe('ExchangeRatesComponent', () => {
+
+
+fdescribe('ExchangeRatesComponent', () => {
   let component: ExchangeRatesComponent;
   let fixture: ComponentFixture<ExchangeRatesComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ExchangeRatesComponent ]
+      declarations: [ExchangeRatesComponent, FormComponent, TableComponent],
+      imports: [ReactiveFormsModule, HttpClientModule],
+      providers: [DatePipe, DecimalPipe]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
